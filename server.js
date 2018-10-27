cec002cf368badda3581cdcdb85241fdafb5b1be
sshrestha25 <gs4l@Shreejeshs-MacBook-Air.local>
2018-10-27 18:58:32 -0400

    projects page

--- server.js
+++ server.js
@@ -12,16 +12,11 @@ app.set('view engine', 'hbs');
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}; ${req.method} ${req.url}`
-  console.log(log);
   fs.appendFile('server.log', log + '\n', err => {
     err && console.log('Unable to append to server.log');
   })
   next();
 });
-
-app.use((req, res, next) => {
-  res.render('maintenance.hbs')
-})
 
 app.use(express.static(__dirname + '/public'))
 
@@ -33,6 +28,12 @@ app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website',
+  })
+});
+
+app.get('/projects', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Project Page',
   })
 });
 
